71f4d60590396d89e9f9b01b7d8fd1e4dde9a0c9
ShamNeko <mkdvlp01@gmail.com>
2024-04-04 20:52:59 +0900

    完成

--- tkinter/calculator.py
+++ tkinter/calculator.py
@@ -1,6 +1,3 @@
-"""
-テンプレート
-"""
 import tkinter as tk
 
 class Application(tk.Frame):
@@ -17,6 +14,8 @@ class Application(tk.Frame):
             ["1", "2", "3", "+"],
             ["00", "0", ".", "="]
         ]
+        self.symbol = ["+", "-", "*", "/"]
+        self.calc_str = ""
 
         # 実行内容
         self.pack() # フレームの配置
@@ -44,7 +43,7 @@ class Application(tk.Frame):
         self.calc_label.pack(anchor=tk.E) # 右揃えで配置
         
         # 結果用ディスプレイ
-        self.ans_label = tk.Label(self.calc_frame, textvariable=self.calc_var, font=("游ゴシック体", "20", "bold"))
+        self.ans_label = tk.Label(self.calc_frame, textvariable=self.ans_var, font=("游ゴシック体", "20", "bold"))
         self.ans_label.pack(anchor=tk.E) # 右揃えで配置
         
     # create_buttonメソッドを定義
@@ -53,6 +52,47 @@ class Application(tk.Frame):
             for x, num in enumerate(row): # button_number リストの各要素内の要素を取得
                 button = tk.Button(self.button_frame, text=num, font=("游ゴシック体", "15", "bold"), width=5, height=2)
                 button.grid(row=y, column=x) # 列、行を指定して配置
+                button.bind("<Button-1>", self.button_clicked) # buttonが左クリックされた場合
+    
+    # ボタンが押された時の処理
+    def button_clicked(self, event):
+        check = event.widget["text"]
+
+        # クリアの場合
+        if check == "C":
+            self.calc_str = ""
+            self.calc_var.set(self.calc_str)
+            self.ans_var.set("")
+        
+        # バックの場合ＯＫ
+        elif check == "B":
+            self.calc_str = self.calc_str[:-1]
+            self.calc_var.set(self.calc_str)
+        
+        # イコールの場合
+        elif check == "=":
+            if self.calc_str !="":
+                if self.calc_str[-1:] in self.symbol: # 最後が記号ならその記号以外を代入
+                    self.calc_str = self.calc_str[:-1]
+            # 計算＆出力
+            res = "=" + str(eval(self.calc_str))
+            self.calc_var.set(self.calc_str)
+            self.ans_var.set(res)
+            self.calc_str = str(eval(self.calc_str))
+        
+        # 記号の場合
+        elif check in self.symbol:
+            if self.calc_str[-1:] not in self.symbol and self.calc_str != "":
+                self.calc_str += check
+                self.calc_var.set(self.calc_str)
+            elif self.calc_str[-1:] in self.symbol:
+                self.calc_str = self.calc_str[:-1] + check
+                self.calc_var.set(self.calc_str)
+        
+        # その他、数字等の場合
+        else:
+            self.calc_str += check
+            self.calc_var.set(self.calc_str)
 
 if __name__ == "__main__":
     root = tk.Tk()
